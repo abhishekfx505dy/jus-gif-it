0a62857e0a21d4145e508eb55f17e9ec1ac0bc46
abhishekfx505dy <abhishek.fx505dy@gmail.com>
2020-07-26 21:25:58 +0530

    Created and Controller class, and updated application.properties.
    In the controller class we are @RequestMapping the /upload, where the
    video is posted. We are saving the video in a File in mp4 format.
    MultipartFile is the type of file posted

---
+++ jus-gif-it/src/main/java/com/sijpat/jusgifit/controller/UploadController.java
@@ -0,0 +1,42 @@
+package com.sijpat.jusgifit.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.spi.LoggerFactoryBinder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
+import ch.qos.logback.classic.Logger;
+
+@RestController
+public class UploadController {
+	
+	
+
+	@Value("${multipart.location}")
+	private String location;
+	
+	@RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.IMAGE_GIF_VALUE)
+	public String upload(@RequestPart("file") MultipartFile file,
+						 @RequestParam("start") int start,
+						 @RequestParam("end") int end,
+						 @RequestParam("speed") int speed,
+						 @RequestParam("repeat") boolean repeat) throws IllegalStateException, IOException
+	{
+		File videoFile = new File(location+ "/" + System.currentTimeMillis()+ ".mp4");
+		file.transferTo(videoFile);
+		System.out.println("Saved file to {}"+ videoFile.getAbsolutePath());
+		
+		return "";
+	}
+}
