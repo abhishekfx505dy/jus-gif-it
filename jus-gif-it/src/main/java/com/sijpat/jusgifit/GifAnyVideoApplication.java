3d953a35c52b1b9acfd00127702383dbeada1381
abhishekfx505dy <abhishek.fx505dy@gmail.com>
2020-07-26 20:56:03 +0530

    1st Commit: (To Master Branch)
    Created a bare bone project from spring initializer, added Spring Web
    and actuator dependencies.
    Added the target folder to .gitignore

---
+++ jus-gif-it/src/main/java/com/sijpat/jusgifit/GifAnyVideoApplication.java
@@ -0,0 +1,13 @@
+package com.sijpat.jusgifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifAnyVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifAnyVideoApplication.class, args);
+	}
+
+}
---
+++ jus-gif-it/src/test/java/com/sijpat/jusgifit/GifAnyVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sijpat.jusgifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifAnyVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
